23dbc32925322614ebefb7c5d8dc1fc558cf4972
Raminta <1989sliekiukasss@gmail.com>
2024-08-07 14:40:55 +0300

    Features modules create + style.css

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,51 +1,51 @@
-import './Features.css';
+import style from './Features.module.css';
 
 export function Features() {
     return (
-        <section className="featuresList">
-            <div className="feature">
-                <div className="icon">💡</div>
-                <h2 className="title">Instant Server Start</h2>
-                <p className="description">
+        <section className={style.featuresList}>
+            <div className={style.feature}>
+                <div className={style.icon}>💡</div>
+                <h2 className={style.title}>Instant Server Start</h2>
+                <p className={style.description}>
                     On demand file serving over native ESM, no bundling
                     required!
                 </p>
             </div>
-            <div className="feature">
-                <div className="icon">⚡️</div>
-                <h2 className="title">Lightning Fast HMR</h2>
-                <p className="description">
+            <div className={style.feature}>
+                <div className={style.icon}>⚡️</div>
+                <h2 className={style.title}>Lightning Fast HMR</h2>
+                <p className={style.description}>
                     Hot Module Replacement (HMR) that stays fast regardless of
                     app size.
                 </p>
             </div>
-            <div className="feature">
-                <div className="icon">⚒️</div>
-                <h2 className="title">Rich Features</h2>
-                <p className="description">
+            <div className={style.feature}>
+                <div className={style.icon}>⚒️</div>
+                <h2 className={style.title}>Rich Features</h2>
+                <p className={style.description}>
                     Out-of-the-box support for TypeScript, JSX, CSS and more.
                 </p>
             </div>
-            <div className="feature">
-                <div className="icon">📦</div>
-                <h2 className="title">Optimized Build</h2>
-                <p className="description">
+            <div className={style.feature}>
+                <div className={style.icon}>📦</div>
+                <h2 className={style.title}>Optimized Build</h2>
+                <p className={style.description}>
                     Pre-configured Rollup build with multi-page and library mode
                     support.
                 </p>
             </div>
-            <div className="feature">
-                <div className="icon">🔩</div>
-                <h2 className="title">Universal Plugins</h2>
-                <p className="description">
+            <div className={style.feature}>
+                <div className={style.icon}>🔩</div>
+                <h2 className={style.title}>Universal Plugins</h2>
+                <p className={style.description}>
                     Rollup-superset plugin interface shared between dev and
                     build.
                 </p>
             </div>
-            <div className="feature">
-                <div className="icon">🔑</div>
-                <h2 className="title">Fully Typed APIs</h2>
-                <p className="description">
+            <div className={style.feature}>
+                <div className={style.icon}>🔑</div>
+                <h2 className={style.title}>Fully Typed APIs</h2>
+                <p className={style.description}>
                     Flexible programmatic APIs with full TypeScript typing.
                 </p>
             </div>
