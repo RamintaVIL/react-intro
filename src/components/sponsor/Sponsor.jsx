86d90eeade8e9a5590a5a1394e061b40fedefeef
Raminta <1989sliekiukasss@gmail.com>
2024-08-07 15:06:35 +0300

    sponsor css

--- src/components/sponsor/Sponsor.jsx
+++ src/components/sponsor/Sponsor.jsx
@@ -1,8 +1,14 @@
+import style from './Sponsor.module.css';
+
 export function Sponsor() {
     return (
-        <section>
-            <a href="#">Sponsor Vite</a>
-            <a href="#">Sponser WHO?</a>
+        <section className={style.sponsors}>
+            <a className={style.btn} href="#">
+                Sponsor Vite
+            </a>
+            <a className={style.btn} href="#">
+                Sponser WHO?
+            </a>
         </section>
     );
 }
